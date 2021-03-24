110963af8aaf7f7d1fedfe7dbaf67b224a222c7f
Penkwe Cjengh <498296816a@gmail.com>
2021-03-24 10:06:56 +0800

    add nettype none

--- defines.v
+++ defines.v
@@ -1,20 +1,23 @@
+`default_nettype none
 //************* Global Macro definition ********************
-`define RstEnable 1'b1			//reset signal
-`define RstDisable 1'b0
-`define ZeroWord 32'h00000000	//zero of 32 bits
-`define WriteEnable 1'b1
-`define WriteDisable 1'b0
-`define ReadEnable 1'b1
-`define ReadDisable 1'b0
-`define AluOpBus 7:0			//ALUop_o width in the Instruction Decipher Stage
-`define AluSelBus 2:0			//ALUsel_o with in ID stage
+`define RstEnable		1'b1			//reset signal
+`define RstDisable		1'b0
+`define ZeroWord		32'h00000000	//zero of 32 bits
+`define WriteEnable		1'b1
+`define WriteDisable	1'b0
+`define ReadEnable		1'b1
+`define ReadDisable		1'b0
+`define AluOpBus		7:0			//ALUop_o width in the Instruction Decipher Stage
+`define AluSelBus		2:0			//ALUsel_o with in ID stage
 //Mind this, valid if 1`b0
-`define InstValid 1'b0			//Valid Instruction
-`define InstInvalid 1'b1
-`define True_v 1'b1				
-`define False_v 1'b0
-`define ChipEnable 1'b1
-`define ChipDisable 1'b0
+`define InstValid		1'b0			//Valid Instruction
+`define InstInvalid		1'b1
+`define Stop			1'b1
+`define NoStop			1'b0
+`define True_v			1'b1				
+`define False_v			1'b0
+`define ChipEnable		1'b1
+`define ChipDisable		1'b0
 
 //************* Macro about specific instruction ************
 //********** Opcode 31~26 **********//
